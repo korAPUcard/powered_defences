1946a0e847e65eec2b004a2dd1a895bf935bd42d
korAPUcard <tmdrnr1157@gmail.com>
2022-08-27 02:38:17 +0900

    Revert "Code totally broken"
    This reverts commit 13d40e7959fc50d870b899fdb1eff2a4f69ba4f4.

--- src/main/java/net/apucsw/powered_defences/block/ForcefieldBarrierVerticalDoubleShieldEmitBlock.java
+++ src/main/java/net/apucsw/powered_defences/block/ForcefieldBarrierVerticalDoubleShieldEmitBlock.java
@@ -151,7 +151,7 @@ public class ForcefieldBarrierVerticalDoubleShieldEmitBlock extends Block
 		int y = pos.getY();
 		int z = pos.getZ();
 
-		FFBV_TickUpdateProcedure.execute(world, x, y, z, blockstate);
+		FFBV_TickUpdateProcedure.execute(world, x, y, z);
 		world.scheduleTick(pos, this, 10);
 	}
 
