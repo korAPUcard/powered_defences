a33fa7049180e3eb1ee3f7d94edd3928989a8387
korAPUcard <tmdrnr1157@gmail.com>
2022-08-27 13:44:40 +0900

    Git Update

--- src/main/java/net/apucsw/powered_defences/PoweredDefencesMod.java
+++ src/main/java/net/apucsw/powered_defences/PoweredDefencesMod.java
@@ -32,15 +32,15 @@ import java.util.function.Function;
 import java.util.function.BiConsumer;
 
 @Mod("powered_defences")
-public class PoweredDefences {
-	public static final Logger LOGGER = LogManager.getLogger(PoweredDefences.class);
+public class PoweredDefencesMod {
+	public static final Logger LOGGER = LogManager.getLogger(PoweredDefencesMod.class);
 	public static final String MODID = "powered_defences";
 	private static final String PROTOCOL_VERSION = "1";
 	public static final SimpleChannel PACKET_HANDLER = NetworkRegistry.newSimpleChannel(new ResourceLocation(MODID, MODID), () -> PROTOCOL_VERSION,
 			PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
 	private static int messageID = 0;
 
-	public PoweredDefences() {
+	public PoweredDefencesMod() {
 		PoweredDefencesModTabs.load();
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 		PoweredDefencesModBlocks.REGISTRY.register(bus);
